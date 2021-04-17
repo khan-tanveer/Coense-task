d273055bc988b851e8519ecf40b564124ae3b937
khan Tanveer <kt224945@gmail.com>
2021-04-17 23:34:16 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyCGB2xQ2gQB_7AjgN9U2AZyjtdvhlwMJrM",
   authDomain: "coense-task1.firebaseapp.com",
   projectId: "coense-task1",
@@ -9,4 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "1026626793077",
   appId: "1:1026626793077:web:3880dd0f67468ac693123c",
   measurementId: "G-PXV68RHWX9",
-};
+});
+
+const db = firebaseApp.firestore();
+
+export default db;
